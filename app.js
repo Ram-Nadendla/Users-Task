7a605137e256a486b2d8092a22145d25ae440b52
Ram-Nadendla <ramnadendla1111@gmail.com>
2021-06-05 09:48:37 +0530

    Removing console and un-used code

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+
+const mongoose = require("mongoose");
+
 const url = "mongodb://localhost:27017/users";
+
 const app = express();
 
-
-mongoose.connect(url,{useNewUrlParser : true})
+mongoose.connect(url, { useNewUrlParser: true });
 
 const connection = mongoose.connection;
-connection.on('open', ()  => {
-    console.log('Database Connected')
+
+connection.on("open", () => {
+  console.log("DB Connected");
 });
+
 app.use(express.json());
-const userRoutes = require('./routes/UserRoutes');
-app.use('/users',userRoutes);
-app.listen(8500, () => {
-    console.log('Server is running in the 8500')
-})
+
+const userRoutes = require("./routes/UserRoutes");
+
+app.use("/users", userRoutes);
+
+app.listen(5488, () => {
+  console.log("node server successfully started on port 5488");
+});
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,77 +1,72 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Users = require('../model/UsersModel');
-const bodyParser = require('body-parser');
-
-// create application/json parser
-const jsonParser = bodyParser.json();
+const Users = require("../model/UsersModel");
+const bodyParser = require("body-parser");
 
 // create application/x-www-form-urlencoded parser
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-router.get('/', async(req,res) =>{
-    try {
-        const users = await Users.find().sort({createdAt : -1})
-        //.skip(3).limit(3);
-        res.send(users)
-    }catch (err){
-        res.send(err)
-    }
-})
-router.post('/',urlencodedParser, async(req,res) =>{
-    console.log(req)
-    console.log(req.query)
-     const userBody = new Users({
-         name : req.body.name,
-         email : req.body.email,
-         mobile : req.body.mobile,
-         address : {
-            street: req.body.street,
-            locality: req.body.locality,
-            city: req.body.city,
-            state: req.body.state,
-            pincode: req.body.pincode,
-            coordinatesType: req.body.coordinatesType,
-           // coordinates: [Number],
-         },
-         createdAt : new Date()
-     });
-    try {
-        const a1 = await userBody.save();
-        res.send(a1)
-    }catch (err){
-        res.send(err)
-    }
-})
-router.get('/:id', async(req,res) =>{
-   try {
-       const user = await Users.findById(req.params.id);
-       res.send(user)
-   }catch (err){
-       res.send(err)
-   }
-})
-router.patch('/:id', async(req,res) =>{
-    console.log(req)
-    try {
-        const user = await Users.findByIdAndUpdate(req.params.id);
-        user.email = req.body.email;
-        const a2 = await user.save();
-        res.send(a2)
-    }catch (err){
-        res.send(err)
-    }
- })
- router.delete('/:id', async(req,res) =>{
-     var deletedRec = {
-         _id : req.params.id
-     }; 
-    try {
-        const user = await Users.deleteOne(deletedRec);
-        res.send('123 Deleted')
-    }catch (err){
-        res.send(err)
-    }
- })
+router.get("/", async (req, res) => {
+  try {
+    const users = await Users.find()
+      .sort({ createdAt: -1 })
+      .skip(req.body.skip)
+      .limit(req.body.limit);
+    res.send(users);
+  } catch (err) {
+    res.send(err);
+  }
+});
+router.post("/", urlencodedParser, async (req, res) => {
+  const userBody = new Users({
+    name: req.body.name,
+    email: req.body.email,
+    mobile: req.body.mobile,
+    address: {
+      street: req.body.street,
+      locality: req.body.locality,
+      city: req.body.city,
+      state: req.body.state,
+      pincode: req.body.pincode,
+      coordinatesType: req.body.coordinatesType,
+      // coordinates: [Number],
+    },
+  });
+  try {
+    const a1 = await userBody.save();
+    res.send(a1);
+  } catch (err) {
+    res.send(err);
+  }
+});
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await Users.findById(req.params.id);
+    res.send(user);
+  } catch (err) {
+    res.send(err);
+  }
+});
+router.patch("/:id", async (req, res) => {
+  try {
+    const user = await Users.findByIdAndUpdate(req.params.id);
+    user.email = req.body.email;
+    const a2 = await user.save();
+    res.send(a2);
+  } catch (err) {
+    res.send(err);
+  }
+});
+router.delete("/:id", async (req, res) => {
+  var deletedRec = {
+    _id: req.params.id,
+  };
+  try {
+    const user = await Users.deleteOne(deletedRec);
+    res.send("Record Deleed");
+  } catch (err) {
+    res.send(err);
+  }
+});
 
-module.exports = router
+module.exports = router;
